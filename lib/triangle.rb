1d6f3eadde044981544084fd7153cb0ac065bb8f
Derric Fragosa <fderric@gmail.com>
2019-12-21 06:50:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require 'pry'
 class Triangle
-  # write code here
+
+  attr_accessor :length_a, :length_b, :length_c
+  
+  def initialize(length1:,length2:,length3:)
+    @length_a = length_a
+    @length_b = length_b
+    @length_c = length_c
+  end
+  binding.pry
+  # solve for Perimeter P = a+b+c
+  def scalene_triangle
+    :scalene
+    begin
+    rescue
+    end 
+  end 
+  
+  def isosceles_triangle
+    :isosceles
+   begin
+    rescue
+    end 
+  end
+  
+  def equilateral_triangle
+    :equilateral
+    begin
+    rescue
+    end 
+  end 
+  
+  
 end
+
